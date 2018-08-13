1e58756627abe3287453cfc319cb3f8a056347d6
Gurjas Singh <gurjassingh92@gmail.com>
2018-08-13 10:50:39 +0530

    Initial commit

---
+++ restaurantmanager/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RestaurantmanagerConfig(AppConfig):
+    name = 'restaurantmanager'
---
+++ restaurantmanager/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+from django.template import loader
+
+
+def index(request):
+    template_name = loader.get_template('index.html')
+    # return HttpResponse("Hello World!")
+    context = {
+        "tag": "This text is from index.html page."
+    }
+    return HttpResponse(template_name.render(context, request))
+
+def aboutus(request):
+    return HttpResponse("Thanks for websiting our website.")
